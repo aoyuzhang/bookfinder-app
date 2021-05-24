8ce6a6d1b1b8956ca1c9ced70b390906e60c387c
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-24 07:33:12 -0700

    added configure file for redux

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,2 +1,8 @@
 import { createStore } from 'redux'
 import createRootReducer from './reducers'
+
+const preloadedState = {}
+
+const store = createStore( createRootReducer() ,
+preloadedState)
+export default store;
