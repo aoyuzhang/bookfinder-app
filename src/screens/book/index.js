d04212cb32d74c79445653a552a723e5b993b932
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-24 07:43:34 -0700

    created Home and Book components

---
+++ src/screens/book/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './book.css'
+
+const Book = () => (
+  <div id = "book" className = "page">
+    <div className = "container">
+
+    </div>
+  </div>
+)
+export default Book;
---
+++ src/screens/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.css'
+
+const Home = () =>(
+  <div id="home" className = "page">
+    <div className = "container">
+    </div>
+  </div>
+)
+export default Home;
